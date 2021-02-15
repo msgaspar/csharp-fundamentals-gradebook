3349a7935a1df8688286f4ab5de1df67d323695e
Matheus Gaspar <msouzagaspar@gmail.com>
2021-02-14 21:44:23 -0300

    add delegates and events for adding grades

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
     public class Book
     {
         public Book(string name)
@@ -16,12 +18,18 @@ namespace GradeBook
             if (grade <= 100 && grade >= 0)
             {
                 grades.Add(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
             }
             else
             {
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
+
+        public event GradeAddedDelegate GradeAdded;
 
         public Statistics GetStatistics()
         {
@@ -66,6 +74,13 @@ namespace GradeBook
         }
 
         private List<double> grades;
-        public string Name;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        readonly string category;
     }
 }
