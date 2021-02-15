a1aaa4342bd4f6555e47aead37a58b80e7b55bfd
Matheus Gaspar <msouzagaspar@gmail.com>
2021-02-15 14:55:45 -0300

    improved user interactione for book creation

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,24 +6,62 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            IBook book = new DiskBook("Scott's Grade Book");
+            IBook book = CreateBook();
             book.GradeAdded += OnGradeAdded;
 
             EnterGrades(book);
 
             var stats = book.GetStatistics();
 
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"");
+            Console.WriteLine($"For the book named {book.Name}:");
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The highest grade is {stats.High:N1}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static IBook CreateBook()
+        {
+            Console.WriteLine("Please enter the student's name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("");
+
+            IBook book;
+
+            while (true)
+            {
+                Console.WriteLine("Where do you want to store the grade book?");
+                Console.WriteLine("  1. In memory");
+                Console.WriteLine("  2. In a .txt file");
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        book = new InMemoryBook($"{name}'s Grade Book");
+                        return book;
+
+                    case "2":
+                        book = new DiskBook($"{name}'s Grade Book");
+                        return book;
+
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        Console.WriteLine("");
+                        break;
+                }
+            }
+
+
+
         }
 
         private static void EnterGrades(IBook book)
         {
             while (true)
             {
+                Console.WriteLine("");
                 Console.WriteLine("Enter a new grade or 'q' to quit:");
                 var input = Console.ReadLine();
 
